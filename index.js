6d24bec584b336263fbf9647880515736cf75040
Jeff Parrish <jeffhparrish@gmail.com>
2015-03-13 12:35:52 -0700

    code cleanup and portno change

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
+'use strict';
 var connect = require('connect');
 var http = require('http');
 var debug = require('debug')('wh');
 var app = connect();
+var format = function (input) { return JSON.stringify(input, null, 2); };
+var port = 3009;
 
-// parse urlencoded request bodies into req.body
+// Parse JSON requests into `req.body`.
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 // Print all incoming requests, then respond to them.
 app.use(function (req, res) {
-    debug('incoming webhook',
-        req.method,
-        req.url,
-        req.body
-    );
+    debug('incoming webhook', req.method, req.url, format(req.body));
     res.end('OK\n');
 });
 
-// create node.js http server and listen on port
+// create node.js http server and listen on port 3001
 var server = http.createServer(app);
 server.on('error', function (err) {
     debug('server error', err);
 });
-server.listen(3001, function () {
-    debug('Respoke webhook server is listening on port 3001')
+server.listen(port, function () {
+    debug('Respoke webhook server is listening on port', port);
 });
